a315795863ed58460e58227e7cd034095c391688
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-10-11 21:29:51 +0600

    Implementing API for Q&A generator feature

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { default: fetch } = require('node-fetch');
+
+
 const port = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const hugging_face_api_key = process.env.HUGGING_FACE_KEY;
+const modelName = "google/flan-t5-small";
+// console.log(hugging_face_api_key)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1k8uoge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -47,7 +54,55 @@ async function run() {
             const newBudget = req.body;
             const result = await budgetCollection.insertOne(newBudget);
             res.send(result);
-        })
+        });
+
+        app.post("/api/answer", async (req, res) => {
+            const { topic } = req.body;
+            console.log(topic)
+
+            if (!topic || !topic.trim()) {
+                return res.status(400).json({ error: "Topic is required." });
+            }
+
+            try {
+                const response = await fetch(`https://api-inference.huggingface.co/models/${modelName}`,
+                    {
+                        method: "POST",
+                        headers: {
+                        Authorization: `Bearer ${hugging_face_api_key}`,
+                        "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ inputs: `Answer this student question clearly:\n\n${topic}` }),
+                    }
+                );
+                console.log(response)
+
+                // Check if HF returned JSON
+                const text = await response.text();
+                let data;
+                
+                try {
+                    data = JSON.parse(text);
+                } 
+                catch {
+                    console.error("HF API returned non-JSON:", text);
+                    return res.status(500).json({ error: "Hugging Face API returned invalid response", details: text });
+                }
+
+                // Extract answer
+                const aiText =
+                data?.[0]?.generated_text ||
+                "⚠️ Sorry, the AI could not generate an answer right now.";
+
+                res.json({ answer: aiText });
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: "Error fetching AI response", details: error.message });
+            }
+        });
+
+
+        
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
